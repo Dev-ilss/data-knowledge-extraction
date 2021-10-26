ec8ea3b202e1891f37c3e39fe5355006be6b96c8
Carlos Ortega <the_king_00carlos@hotmail.com>
2021-10-25 20:03:54 -0600

    limpieza de datos

--- tarea_2.3/main.py
+++ tarea_2.3/main.py
@@ -2,12 +2,24 @@ import pandas as pd
 
 pd.set_option('display.max_columns', None)
 
-data = pd.read_csv('./international-migration-March-2021-citizenship-by-visa-by-country-of-last-permanent-residence.csv');
+data = pd.read_csv('./international-migration-March-2021-citizenship-by-visa-by-country-of-last-permanent-residence.csv')
 
 # Podemos mostrar las columnas con
 # data.columns
 
 # Y las podemos seleccionar o crear un subset
-# data_subset = data[...columnas]
+data_subset = data[['year_month','month_of_release','citizenship','visa', 'country_of_residence', 'estimate']]
 
-print(data.isna().sum());
+#print(len(data_subset))
+
+# Removemos los valores TOTAL en la columna visa
+data_subset = data_subset[data_subset['visa'] != 'TOTAL']
+# Removemos los valores TOTAL en la columna country_of_residence
+data_subset = data_subset[data_subset['country_of_residence'] != 'TOTAL']
+# Removemos los valores TOTAL en la columna citizenship
+data_subset = data_subset[data_subset['citizenship'] != 'TOTAL']
+
+#print(len(data_subset))
+
+print(data_subset.head(6))
+
